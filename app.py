4019697f82a89a8910c063dd01545d761252e1c4
Cedric Arisdakessian <carisdakessian@rometx.com>
2023-12-27 19:36:33 +0100

    cleanup

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ st.set_page_config(layout="wide")
 
 st.title("Coinche-moi STP")
 
-url = "https://raw.githubusercontent.com/Puumanamana/coinche/main/assets/cards"
+# url = "https://raw.githubusercontent.com/Puumanamana/coinche/main/assets/cards"
 
-cards = [Path(url, f"{suit}_{value}.png")
+cards = [Path("assets", "cards", f"{suit}_{value}.png")
          for suit in ["hearts", "diamonds", "spades", "clubs"]
          for value in ["7", "8", "9", "10", "jack", "queen", "king", "ace"]]
 suits = {x: x.stem.split('_')[0] for x in cards}
@@ -24,6 +24,7 @@ c.execute("CREATE TABLE IF NOT EXISTS coinche(hand, guess, color, comment)")
 conn.commit()
 conn.close()
 
+
 def get_db():
     df = pd.read_sql(
         "SELECT * FROM coinche",
@@ -31,21 +32,24 @@ def get_db():
     )
     return df
 
-def save_to_db():
+def save_to_db(guess, suit, comment):
+    if suit is None:
+        st.warning("Veuillez choisir une couleur")
+        return
+
     db_entry = (
         ",".join(x.stem for x in st.session_state.hand),
-        st.session_state.guess,
-        st.session_state.color,
-        st.session_state.comment
+        guess, suit, comment
     )
     conn = sqlite3.connect('coinche.db')
     c = conn.cursor()
     c.execute("INSERT INTO coinche (hand, guess, color, comment) VALUES (?, ?, ?, ?)", db_entry)
     conn.commit()
+
     # Close connection
     conn.close()
 
-def random_hand():
+def update_hand():
     st.session_state["hand"] = np.random.choice(cards, 8, replace=False)
 
 def sort_hand(hand):
@@ -75,36 +79,39 @@ def sort_hand(hand):
 tabs = st.tabs(["Jouer", "Historique"])
 
 with tabs[0]:
-    show_next = st.button("Prochaine main", on_click=random_hand, type="primary")
+    update_hand = st.button("Prochaine main", on_click=update_hand, type="primary")
 
     if not "hand" in st.session_state:
         st.stop()
 
     st.image(sort_hand(st.session_state.hand), width=170)
 
-    with st.form("mise"):
-        cols = st.columns([1, 1, 2])
-        with cols[0]:
-            st.number_input(
+    layout, _ = st.columns([1, 1])
+    with layout:
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            guess = st.number_input(
                 "Choisir une annonce : ", key="guess",
                 min_value=0, max_value=270, value=80, step=10
             )
-            st.selectbox(
+        with col2:
+            suit = st.selectbox(
                 "Choisir une couleur : ",
                 ["Coeur", "Carreau", "Pique", "Trèfle", "Sans atout", "Tout atout"],
                 index=None, key="color"
             )
-            st.text_area("Ajouter un commentaire :", key="comment")
-            submitted = st.form_submit_button("Valider", on_click=save_to_db)
 
-        if submitted:
-            st.write(f"Mise : {st.session_state.guess}")
+        comment = st.text_area("Ajouter un commentaire :", key="comment")
+
+        submitted = st.button("Valider", on_click=save_to_db, args=(guess, suit, comment))
+
+        if submitted and suit:
+            st.success(f"Mise enregistrée : {guess} {symbols[suit.lower()]}")
 
 with tabs[1]:
     df = get_db()
 
     for row in df.itertuples():
-        # hand = [Path(url, f"{card}.png") for card in row.hand.split(",")]
         hand = [Path("assets", "cards", f"{card}.png") for card in row.hand.split(",")]
         st.image(sort_hand(hand), width=170)
         st.metric(label="Mise", value=f"{row.guess} {symbols[row.color.lower()]}")
